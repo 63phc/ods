d1bbb89f96f72489791a7400a4326ea6d5433aae
63phc <pavel.burns@gmail.com>
2019-12-08 20:03:51 +0100

    [docker]

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
+if [[ ${POSTGRES_HOST} ]]; then
+    export PGPASSWORD=${POSTGRES_PASSWORD}
+    until psql -h $POSTGRES_HOST -U $POSTGRES_USER -d $POSTGRES_NAME -c '\l'; do
+      >&2 echo "Postgres is unavailable - sleeping"
+      sleep 1
+    done
+    >&2 echo "Postgres is up - continuing"
+    psql -h $POSTGRES_HOST -U $POSTGRES_USER -d template1 -c 'create extension hstore;'
+    unset PGPASSWORD
+fi
+
+
 echo "RUN migrate collectstatic compilemessages"
 python manage.py migrate
 python manage.py collectstatic --no-input
 python manage.py compilemessages
-gunicorn src.wsgi:application -b 0.0.0.0:8080
+gunicorn src.wsgi:application -b 0.0.0.0:80
--- src/settings.py
+++ src/settings.py
@@ -47,23 +47,23 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'src.wsgi.application'
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'ods_test',
-#         'USER': 'ods_test',
-#         'PASSWORD': 'ods_test',
-#         'HOST': 'localhost',
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ods_test',
+        'USER': 'ods_test',
+        'PASSWORD': 'ods_test',
+        'HOST': 'postgres',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
